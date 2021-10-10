4fc0a534502197db6b313e56bffb331762ed4721
matthieu.plessier <plessier.matthieu@gmail.com>
2021-10-10 14:58:36 +0200

    maj

--- utils/dbConnect.php
+++ utils/dbConnect.php
@@ -1,4 +1,5 @@
 <?php
+// Inclusion du fichier config
 require_once dirname(__FILE__).'/../config/config.php';
 // SINGLETON
 
@@ -7,13 +8,15 @@ class Database{
 
     private static $_pdo;
 
+    // Méthodes de création de l'instance
     public static function getInstance()
     {
         try {
+            // On test si l'instance est unique
             if(is_null(self::$_pdo)){
-                self::$_pdo = new PDO(DSN,LOGIN, PASSWORD);
-                self::$_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                self::$_pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);//defini la méthode de retour fetch
+                self::$_pdo = new PDO(DSN,LOGIN, PASSWORD); //Connection avec la PDO
+                self::$_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Active le Catch des erreurs
+                self::$_pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);//Defini la méthode de retour fetch
 
             }
             return self::$_pdo;
